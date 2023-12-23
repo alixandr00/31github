1ecad652e94f37fc7bd2ad94e517611b6916dd22
Ali <alisamatov106@gmail.com>
2023-12-23 21:06:14 +0600

    58

--- src/components/UI/snackbar/Snackbar.jsx
+++ src/components/UI/snackbar/Snackbar.jsx
@@ -23,7 +23,7 @@ const CustomToast = ({ message, additionalMessage, severity }) => {
   };
   let secondWordStyle = {
     color: "",
-    fontSize: "57",
+    fontSize: "58",
   };
 
   if (severity === "success") {
